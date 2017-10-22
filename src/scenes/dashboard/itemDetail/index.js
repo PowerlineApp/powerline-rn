dac1595e3b20e27de13031823df6eae507d94ba6
Felipe Piubello <felipe.appmasters@gmail.com>
2017-10-21 22:06:04 -0200

    adjusted keyboard height in comment modal

--- src/scenes/dashboard/itemDetail/index.js
+++ src/scenes/dashboard/itemDetail/index.js
@@ -6,6 +6,7 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {ScrollView} from 'react-native';
 import { Container, Header, Title, Textarea, Content, Text, Button, Icon, Left, Right, Body, Thumbnail, CardItem, Label, List, ListItem, Item, Input } from 'native-base';
 import { Image, View, StyleSheet, TouchableOpacity, Platform, KeyboardAvoidingView, Keyboard, TextInput, ListView } from 'react-native';
 import { Actions } from 'react-native-router-flux';
@@ -495,22 +496,26 @@ class ItemDetail extends Component {
                                     width: WINDOW_WIDTH,
                                     // height: this.state.visibleHeight,
                                     // this needs adjustment for android / ios - doesnt work well for android with the suggestionbox
-                                    height: WINDOW_HEIGHT /2 + 50
+                                    minHeight: Platform.OS ==='android' ? 50 :  WINDOW_HEIGHT/2 + 50
                                 }}>
+                                        <ScrollView keyboardShoulPersisTaps>
                                         <SuggestionBox substitute={(mention) => this.substitute(mention)} displaySuggestionBox={this.state.displaySuggestionBox} userList={this.state.suggestionList} />
+                                        </ScrollView>
                                         <CardItem>
                                             <Left>
                                             <Thumbnail small source={thumbnail ? { uri: thumbnail+'&w=50&h=50&auto=compress,format,q=95' } : require("img/blank_person.png")} defaultSource={require("img/blank_person.png")} />
                                             <Body>
                                                 <TextInput
                                                     autoFocus
+                                                    keyboardShoulPersisTaps
                                                     style={styles.commentInput}
                                                     ref={this.onCommentInputRef}
                                                     placeholder={this.state.placeholderTitle}
                                                     defaultValue={this.state.defaultInputValue}
                                                     onChangeText={commentText => this.setState({ commentText })}
                                                     onSelectionChange={(e) => this.onSelectionChange(e)}
-
+                                                    multiline
+                                                    numberOfLines={3}
                                                 />
                                             </Body>
                                             <Right style={{ flex: 0.3 }}>
