a7267878a5d2e5a39e5b4200410f28cb94360ef6
Christian Tucker <christian.tucker@outlook.com>
2018-04-09 14:14:48 -0500

    Finished layout for settings icons

--- src/scenes/settings/index.js
+++ src/scenes/settings/index.js
@@ -1,4 +1,39 @@
 import React from "react";
-import { View } from "react-native";
+import { Text, View, TouchableHighlight } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
-export default () => <View style={{ flex: 1, backgroundColor: "red" }} />;
+import styles from "./styles";
+
+const icons = [
+  { icon: "md-lock", label: "Privacy Settings", onPress: () => {} },
+  { icon: "md-options", label: "Customization Code", onPress: () => {} },
+  { icon: "md-eye-off", label: "Blocked Users", onPress: () => {} },
+  { icon: "md-card", label: "Payment Info", onPress: () => {} },
+  { icon: "md-notifications", label: "Notifications", onPress: () => {} },
+  { icon: "md-person", label: "My Profile", onPress: () => {} },
+  { icon: "md-mail", label: "Contact Us", onPress: () => {} },
+  { icon: "md-list", label: "Terms of Service", onPress: () => {} },
+  { icon: "md-list", label: "Privacy Policy", onPress: () => {} }
+];
+
+export default () => (
+  <View style={styles.container}>
+    <View style={styles.wrapper}>
+      {icons.map((icon, index) => {
+        return (
+          <View style={styles.item}>
+            <TouchableHighlight
+              key={index}
+              style={styles.iconContainer}
+              underlayColor="#474b87"
+              onPress={icon.onPress}
+            >
+              <Icon name={icon.icon} size={40} color="#121768" />
+            </TouchableHighlight>
+            <Text style={styles.label}>{icon.label}</Text>
+          </View>
+        );
+      })}
+    </View>
+  </View>
+);
--- src/scenes/settings/styles.js
+++ src/scenes/settings/styles.js
@@ -1,1 +1,34 @@
-export default {};
+import ScaleSheet from "react-native-scalesheet";
+
+export default ScaleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  wrapper: {
+    flexWrap: "wrap",
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  item: {
+    width: "30%",
+    height: "25vh",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginTop: "3%"
+  },
+  iconContainer: {
+    width: "15vh",
+    height: "15vh",
+    borderRadius: "7.5vh",
+    backgroundColor: "#5559A1",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  label: {
+    textAlign: "center",
+    marginTop: 10,
+    fontSize: 15
+  }
+});
