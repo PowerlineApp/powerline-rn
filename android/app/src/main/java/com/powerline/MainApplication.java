f6e9b9f908cc79cd24ca7e21ceb107a259927f0f
Phoenix <phoenix214@protonmail.com>
2018-09-21 00:44:04 +0600

    cleaning up iOS

--- android/app/src/main/java/com/powerline/MainApplication.java
+++ android/app/src/main/java/com/powerline/MainApplication.java
@@ -3,6 +3,23 @@ package com.powerline;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.gettipsi.stripe.StripeReactPackage;
+import com.inprogress.reactnativeyoutube.ReactNativeYouTube;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.github.alinz.rnsk.RNSKPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.alinz.parkerdan.shareextension.SharePackage;
+import cl.json.RNSharePackage;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
+import com.kevinejohn.RNMixpanel.RNMixpanel;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import com.rnfs.RNFSPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.centaurwarchief.smslistener.SmsListenerPackage;
+import com.vonovak.AddCalendarEventPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +39,24 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new StripeReactPackage(),
+            new ReactNativeYouTube(),
+            new VectorIconsPackage(),
+            new RNSKPackage(),
+            new SplashScreenReactPackage(),
+            new SharePackage(),
+            new RNSharePackage(),
+            new ReactNativeOneSignalPackage(),
+            new RNMixpanel(),
+            new LinearGradientPackage(),
+            new PickerPackage(),
+            new RNFSPackage(),
+            new RNFetchBlobPackage(),
+            new FBSDKPackage(),
+            new RNDeviceInfo(),
+            new SmsListenerPackage(),
+            new AddCalendarEventPackage()
       );
     }
 
