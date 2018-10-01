60c87a4e5c11fd7940cf9c98dffc90a69f6218bf
Phoenix <phoenix214@protonmail.com>
2018-09-30 20:48:24 -0700

    fixed the bundling issue - working on embeded library

--- ios/powerline/AppDelegate.m
+++ ios/powerline/AppDelegate.m
@@ -24,7 +24,7 @@
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-
+//jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"]; 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"Powerline"
                                                initialProperties:nil
