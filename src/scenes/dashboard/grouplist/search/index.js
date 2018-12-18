70ac34a61b90f27d9dd8b42064ffd97ea76d3098
Phoenix <phoenix214@protonmail.com>
2018-12-18 13:55:55 +0600

    minor bug fixing

--- src/scenes/dashboard/grouplist/search/index.js
+++ src/scenes/dashboard/grouplist/search/index.js
@@ -186,7 +186,7 @@ class GroupSearch extends Component{
                                 <Text style={styles.itemHeaderText}>Results</Text>
                             </ListItem> : null}
                         {
-                            this.state.searchResults.length > 0 ?
+                            this.state.searchResults && this.state.searchResults.length > 0 ?
                                 <View>
                                     {
                                     this.state.searchResults.map((group, index) => {
